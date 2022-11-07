ed0be6f80353c068cb03ff0a8792d313b6ae7fad
2KNG <khkim1207@naver.com>
2022-11-07 14:05:23 +0900

    add broadcast

--- gocart_websocket_test.py
+++ gocart_websocket_test.py
@@ -4,11 +4,13 @@ import threading
 
 robot_message = 0
 
+ip = "0.0.0.0"
+# ip = "etom.run"
 
 def ws_thread():
     async def connect():
         # 웹 소켓에 접속을 합니다.
-        async with websockets.connect("ws://etom.run:7712", ping_interval=None) as websocket:
+        async with websockets.connect("ws://{}:7712".format(ip), ping_interval=None) as websocket:
             await websocket.send("mac connected")
             while True:
                 data = await websocket.recv()
--- gocart_websocket_test_server.py
+++ gocart_websocket_test_server.py
@@ -1,17 +1,38 @@
 import asyncio
 import websockets
 
-data = 0
 
-async def accept(websocket):
-    await websocket.send("Welcome to ETOM")
-    while True:
-        global data
-        data = await websocket.recv()
-        if data:
-            await websocket.send(data)
-            print(data)
+class EtomServer:
+    def __init__(self):
+        self.clients = []
+        self.data = 0
+    async def echo(self, websocket):
+        self.clients.append(websocket)
+        await websocket.send("Welcome to ETOM")
+        async for message in websocket:
+            for client in self.clients:
+                await client.send(message)
+            # websocket.broadcast(self.clients, message)
 
-start_server = websockets.serve(accept, "0.0.0.0", 7712)
+
+# async def handler(websocket):
+#     connected = {websocket}
+#
+# async def echo(websocket):
+#     await websocket.send("connected")
+#     async for message in websocket:
+#         websockets.broadcast()
+#         await websocket.send(message)
+#
+# async def accept(websocket):
+#     await websocket.send("Welcome to ETOM")
+#     while True:
+#         global data
+#         data = await websocket.recv()
+#         if data:
+#             await websocket.send(data)
+#             print(data)
+etom = EtomServer()
+start_server = websockets.serve(etom.echo, "0.0.0.0", 7712)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
